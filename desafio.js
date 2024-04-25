b1048fe7969edbf0ce42a60637f9717d03ffcfb0
RodrigoSouzaB <rodrigo.bomfim.souza@gmail.com>
2024-04-25 09:57:20 -0300

    first commit

---
+++ desafio.js
@@ -0,0 +1,38 @@
+console.log("Digite o nome do seu herói")
+let nickname = "Rodrigo"
+console.log("Bem vindo, Herói " + nickname)
+let rank = 1
+let missao = "Slime"
+let experienciaInicial = 1
+if (missao === "Slime"){
+    let experiencia = experienciaInicial + 1000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+} else if(missao === "Lobo selvagem"){
+    let experiencia = experienciaInicial + 2000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+} else if(missao === "Urso Selvagem"){
+    let experiencia = experienciaInicial + 3000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+} else if(missao === "Urso demoniaco"){
+    let experiencia = experienciaInicial + 4000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+} else if(missao === "Vampiro menor"){
+    let experiencia = experienciaInicial + 5000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+} else if(missao === "Vampiro maior"){
+    let experiencia = experienciaInicial + 6000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+} else if(missao === "Lorde Vampiro"){
+    let experiencia = experienciaInicial + 7000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+} else if(missao === "Demonio"){
+    let experiencia = experienciaInicial + 8000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+} else if(missao === "Lorde Demonio"){
+    let experiencia = experienciaInicial + 9000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+} else if (missao === "Rei Demonio"){
+    let experiencia = experienciaInicial + 10000
+    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+}
+
