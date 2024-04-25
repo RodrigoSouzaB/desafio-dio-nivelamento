e39fa6d8d192ce67e750f79d4d2f039cc91396da
RodrigoSouzaB <rodrigo.bomfim.souza@gmail.com>
2024-04-25 11:14:12 -0300

    atualização

--- desafio.js
+++ desafio.js
@@ -1,38 +1,39 @@
 console.log("Digite o nome do seu herói")
 let nickname = "Rodrigo"
 console.log("Bem vindo, Herói " + nickname)
-let rank = 1
 let missao = "Slime"
 let experienciaInicial = 1
-if (missao === "Slime"){
-    let experiencia = experienciaInicial + 1000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
-} else if(missao === "Lobo selvagem"){
-    let experiencia = experienciaInicial + 2000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
-} else if(missao === "Urso Selvagem"){
-    let experiencia = experienciaInicial + 3000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
-} else if(missao === "Urso demoniaco"){
-    let experiencia = experienciaInicial + 4000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
-} else if(missao === "Vampiro menor"){
-    let experiencia = experienciaInicial + 5000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
-} else if(missao === "Vampiro maior"){
-    let experiencia = experienciaInicial + 6000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
-} else if(missao === "Lorde Vampiro"){
-    let experiencia = experienciaInicial + 7000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
-} else if(missao === "Demonio"){
-    let experiencia = experienciaInicial + 8000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
-} else if(missao === "Lorde Demonio"){
-    let experiencia = experienciaInicial + 9000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
-} else if (missao === "Rei Demonio"){
-    let experiencia = experienciaInicial + 10000
-    console.log(nickname + "derrotou " + missao + "e recebeu " + experiencia + "de experiencia")
+if (missao === "Slime") {
+    let nivel = experienciaInicial + 500
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+} else if (missao === "Lobo selvagem") {
+    let nivel = experienciaInicial + 2000
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+} else if (missao === "Urso Selvagem") {
+    let nivel = experienciaInicial + 3000
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+} else if (missao === "Urso demoniaco") {
+    let nivel = experienciaInicial + 4000
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+} else if (missao === "Vampiro menor") {
+    let nivel = experienciaInicial + 5000
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+} else if (missao === "Vampiro maior") {
+    let nivel = experienciaInicial + 6000
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+} else if (missao === "Lorde Vampiro") {
+    let nivel = experienciaInicial + 7000
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+} else if (missao === "Demonio") {
+    let nivel = experienciaInicial + 8000
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+} else if (missao === "Lorde Demonio") {
+    let nivel = experienciaInicial + 9000
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+} else if (missao === "Rei Demonio") {
+    let nivel = experienciaInicial + 10000
+    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
 }
+
+
 
