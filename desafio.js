0a542001d192b7fcbee2ce7a372322c144d6b19f
RodrigoSouzaB <rodrigo.bomfim.souza@gmail.com>
2024-04-25 16:33:28 -0300

    projeto realizado

--- desafio.js
+++ desafio.js
@@ -1,39 +1,19 @@
-console.log("Digite o nome do seu herói")
 let nickname = "Rodrigo"
-console.log("Bem vindo, Herói " + nickname)
-let missao = "Slime"
-let experienciaInicial = 1
-if (missao === "Slime") {
-    let nivel = experienciaInicial + 500
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
-} else if (missao === "Lobo selvagem") {
-    let nivel = experienciaInicial + 2000
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
-} else if (missao === "Urso Selvagem") {
-    let nivel = experienciaInicial + 3000
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
-} else if (missao === "Urso demoniaco") {
-    let nivel = experienciaInicial + 4000
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
-} else if (missao === "Vampiro menor") {
-    let nivel = experienciaInicial + 5000
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
-} else if (missao === "Vampiro maior") {
-    let nivel = experienciaInicial + 6000
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
-} else if (missao === "Lorde Vampiro") {
-    let nivel = experienciaInicial + 7000
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
-} else if (missao === "Demonio") {
-    let nivel = experienciaInicial + 8000
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
-} else if (missao === "Lorde Demonio") {
-    let nivel = experienciaInicial + 9000
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
-} else if (missao === "Rei Demonio") {
-    let nivel = experienciaInicial + 10000
-    console.log(nickname + " derrotou " + missao + " e recebeu " + nivel + " de experiencia")
+let exp = 1311
+if (exp <= 1000){
+    console.log("O herói " + nickname + " Alcançou o nivel FERRO")
+} else if(exp >= 1001 && exp <=2000){
+    console.log("O herói " + nickname + " Alcançou o nivel BRONZE")
+} else if(exp >= 2001 && exp <=5000){
+    console.log("O herói " + nickname + " Alcançou o nivel PRATA")
+} else if(exp >= 5001 && exp <=7000){
+    console.log("O herói " + nickname + " Alcançou o nivel OURO")
+} else if(exp >= 7001 && exp <=8000){
+    console.log("O herói " + nickname + " Alcançou o nivel PLATINA")
+}else if(exp >= 8001 && exp <=9000){
+    console.log("O herói " + nickname + " Alcançou o nivel ASCENDENTE")
+} else if(exp >= 9001 && exp <=10000){
+    console.log("O herói " + nickname + " Alcançou o nivel IMORTAL")
+} else if(exp > 10000){
+    console.log("O herói " + nickname + " Alcançou o nivel RADIANTE")
 }
-
-
-
